9411d4c81ed1e03928d873a21de7a696d917e6cf
mathboam <dev@remoteli.co.uk>
2023-11-03 02:20:56 +0000

    updated env and api root

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,7 +3,7 @@ import { logout } from "./auth.service";
 import JwtService from "./jwt.service";
 
 const token = JwtService.getItem("token");
-axios.defaults.baseURL = "https://frontend-test-api.mvm-tech.xyz/api/";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL as string;
 axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
 
 const headers = () => {
@@ -29,6 +29,6 @@ axios.interceptors.response.use(
 export const dispatch = (data: unknown, url: string) =>
   axios.post(url, data, headers());
 export const get = (url: string) => axios.get(url, headers());
-export const put = (data: unknown, url: string) => 
-    axios.put(url, data, headers());
+export const put = (data: unknown, url: string) =>
+  axios.put(url, data, headers());
 export const remove = (url: string) => axios.delete(url, headers());
